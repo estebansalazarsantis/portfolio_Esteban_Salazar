cf3e2802c062564a4b959313683ce3b1a1d074aa
Esteban Salazar <esteban.salazar.santis@gmail.com>
2025-09-14 20:06:29 -0300

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -63,7 +63,7 @@ function Home() {
           ref={h11}
           className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
         >
-          Hi,👋<br></br>My Name is<br></br>
+          Hola,👋<br></br>Mi nombre es<br></br>
         </h1>
         <h1
           ref={h12}
