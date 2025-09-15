eb6dc93c48fa4e0c56b218ad45f6dcc7cf9651f1
Esteban Salazar <esteban.salazar.santis@gmail.com>
2025-09-14 21:35:00 -0300

    Update About.js

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -7,13 +7,13 @@ function About() {
     <main className="container mx-auto max-width pt-10 pb-20 ">
       <section>
         <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
-          About Me
+          Acerca de mí
         </h1>
         <p className="text-content py-8 lg:max-w-3xl">{personalDetails.about}</p>
       </section>
       <section>
         <h1 className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
-          Work Experience
+          Experiencia laboral
         </h1>
         {React.Children.toArray(
           workDetails.map(({ Position, Company, Location, Type, Duration }) => (
@@ -29,7 +29,7 @@ function About() {
       </section>
       <section>
         <h1 className="text-2xl pt-10 text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold">
-          Education
+          Estudios
         </h1>
         {React.Children.toArray(
           eduDetails.map(({ Position, Company, Location, Type, Duration }) => (
